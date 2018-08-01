970d29747c5c1929701cce5c8a7514bea79bef36
Moudou <dplayerd@gmail.com>
2018-08-01 20:34:27 +0800

    commit

---
+++ MVC3/Controllers/CalendarController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC3.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditPage()
+        {
+            return View();
+        }
+
+        public ActionResult Created()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC3/Models/Calendar/CalendarVIewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC3.Models.Calendar
+{
+    public class CalendarVIewModel
+    {
+        public int CalendarID { get; set; }
+        public string CalendarName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
