02441ec8b1c44f01f0184d120ef5e820f3131df2
Moudou <dplayerd@gmail.com>
2018-09-10 23:40:03 +0800

    加入頁面和模組關係物件；

---
+++ Settings/PageModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Settings
+{
+    public class PageModule
+    {
+        #region " Page & Module "
+        public int PageModuleID { get; set; }
+
+        public int MenuID { get; set; }
+
+        public int ModuleID { get; set; }
+        #endregion
+
+
+        #region "Info"
+        public int ContainerID { get; set; }
+
+        public int SortingOrder { get; set; }
+        #endregion
+    }
+}
