8e1d360861f0a9b81edc58280d7ed52ad58e08f2
Moudou <will@kingfor.com>
2018-09-13 09:52:16 +0800

    整理至可做基礎 DEMO ；

--- WebApplication4/Controllers/HtmlModuleController.cs
+++ WebApplication4/Controllers/HtmlModuleController.cs
@@ -8,8 +8,27 @@ namespace WebApplication4.Controllers
 {
     public class HtmlModuleController : Controller
     {
+
+        string FilePath = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/");
+
         public ActionResult ViewDetail(int ModuleInstanceID = 0)
         {
+            if (ModuleInstanceID <= 0)
+                return new EmptyResult();
+
+
+            string IOPath = FilePath + "HTML_" + ModuleInstanceID + ".html";
+
+
+            if (!System.IO.File.Exists(IOPath))
+                return new EmptyResult();
+
+
+            string htmlContent =
+                System.IO.File.ReadAllText(IOPath);
+
+
+            this.ViewData["Context"] = htmlContent;
             return PartialView();
         }
     }
